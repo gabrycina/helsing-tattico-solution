303a8b4341f75f6d559e699379ce1ed0c4984da6
DannyAntonelli <dannyantonelli2002@gmail.com>
2025-05-10 15:42:21 +0100

    Add env token

--- src/main.py
+++ src/main.py
@@ -3,28 +3,38 @@ import simulation_pb2
 import simulation_pb2_grpc
 from google.protobuf.empty_pb2 import Empty
 from google.protobuf.wrappers_pb2 import StringValue
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 # Server address and port
 SERVER_ADDRESS = "172.237.124.96:21234"
-TOKEN = "123454"
+TOKEN = os.environ["token"]
+
 
 def start_simulation():
     with grpc.insecure_channel(SERVER_ADDRESS) as channel:
         stub = simulation_pb2_grpc.SimulationStub(channel)
-        
+
         response = stub.Start(Empty(), metadata=[("authorization", f"bearer {TOKEN}")])
         print("Simulation started with parameters:")
         print(response)
         return response
 
+
 def get_simulation_status(simulation_id):
     with grpc.insecure_channel(SERVER_ADDRESS) as channel:
         stub = simulation_pb2_grpc.SimulationStub(channel)
-        
+
         request = StringValue(value=simulation_id)
-        response = stub.GetSimulationStatus(request, metadata=[("authorization", f"bearer {TOKEN}")])
+        response = stub.GetSimulationStatus(
+            request, metadata=[("authorization", f"bearer {TOKEN}")]
+        )
         print("Simulation status:")
         print(response)
+
 
 if __name__ == "__main__":
     response = start_simulation()
